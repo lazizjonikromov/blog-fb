f952e846effe9ec116be1f53d3f1a3ac97d6ea2c
Laziz <lazizjonweb@gmail.com>
2024-01-13 14:28:46 +0500

    demo header is added

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const nav = [
+    {
+        title: "Our story",
+        path: "/",
+    },
+    {
+        title: "Membership",
+        path: "/",
+    },
+    {
+        title: "Write",
+        path: "/",
+    },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: "rgba(0,0,0,0.8)",
+        banner: "rgb(255, 192, 23)",
+      },
+      fontFamily: {
+        title: `gt-super, Georgia, Cambridge, Times New Roman, Times, serif`,
+        texts: `sohne, Helvetica Neue, Helvetica, Arial, sans-serif`, 
+      },
+      gridTemplateColumns: {
+        card: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
+};  
