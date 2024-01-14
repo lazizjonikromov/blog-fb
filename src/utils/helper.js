36ff677611603c9a11e2cfc29c455b21a25f083a
Laziz <lazizjonweb@gmail.com>
2024-01-14 17:26:32 +0500

    Home modal is added

---
+++ src/utils/helper.js
@@ -0,0 +1,7 @@
+export const secretEmail = (email) => {
+    const [username, domain] = email.split("@");
+
+    const secretUser = username.substring(0, 2) + "*".repeat(username.length - 2);
+
+    return `${secretUser}@${domain}`;
+}
