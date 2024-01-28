aea4a2a36ab3d4bc8ab152e47b3608e88153382b
Laziz <lazizjonweb@gmail.com>
2024-01-28 17:33:23 +0500

    saved post has been implemented

--- src/components/Common/Post/Actions/SavedPost.jsx
+++ src/components/Common/Post/Actions/SavedPost.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
-import { CiSaveDown2 } from "react-icons/ci"
+import React, { useEffect, useState } from "react";
+import { CiSaveDown2 } from "react-icons/ci";
+import { Blog } from "../../../../Context/Context";
+import { db } from "../../../../firebase/firebase";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import UseSingleFetch from "../../../hooks/UseSingleFetch";
 
 const SavedPost = ({ post }) => {
+  const [isSaved, setIsSaved] = useState(false);
+  const { currentUser } = Blog();
+  const { data, loading } = UseSingleFetch(
+    "users",
+    currentUser?.uid,
+    "savePost"
+  );
+
+  // console.log(data);
+
+  useEffect(() => {
+    setIsSaved(data && data.find((item) => item.id === post.id));
+  }, [data, post?.id]);
+  
+
+  const hadleSave = async () => {
+    try {
+      if (currentUser) {
+        const saveRef = doc(
+          db,
+          "users",
+          currentUser?.uid,
+          "savePost",
+          post?.id
+        );
+
+        if (isSaved) {
+          await deleteDoc(saveRef);
+          toast.success("Post has been unsaved");
+        } else {
+          await setDoc(saveRef, {
+            ...post,
+          });
+          toast.success("Post has been Saved");
+        }
+      }
+    } catch (error) {}
+  };
+
   return (
     <>
-        <button className='hover:opacity-60'>
-            <CiSaveDown2 className='text-2xl pointer-event-none'/>
-        </button>
+      <button onClick={hadleSave} className="hover:opacity-60">
+        <CiSaveDown2
+          className={`text-2xl pointer-event-none ${
+            isSaved ? "text-yellow-600" : ""
+          } `}
+        />
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default SavedPost
+export default SavedPost;
--- src/components/Home/Profile/Activities/ProfileHome.jsx
+++ src/components/Home/Profile/Activities/ProfileHome.jsx
@@ -1,51 +1,28 @@
-import React from 'react'
+import React from "react";
+import UseFetch from "../../../hooks/UseFetch";
+import Loading from "../../../Loading/Loading";
+import PostsCard from "../../../Common/Post/PostsCard";
 
-const ProfileHome = () => {
+const ProfileHome = ({ getUserData }) => {
+  const { data, loading } = UseFetch("posts");
+  const userPost =
+    data && data?.filter((post) => post.userId === getUserData?.userId);
+  // console.log(userPost);
   return (
-    <div>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
-        <h1>ProfileHome</h1>
+    <div className="flex flex-col gap-5 mb-[4rem]">
+      {userPost.length === 0 && (
+        <p className="text-gray-500">
+          <span className="capitalize">{getUserData?.username}</span> has no
+          posts
+        </p>
+      )}
+      {loading ? (
+        <Loading />
+      ) : (
+        data?.map((post, i) => <PostsCard post={post} key={i} />)
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProfileHome
+export default ProfileHome;
