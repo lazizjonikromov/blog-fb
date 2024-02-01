0f04204101833b981d8eeba683985504096fa2a9
Laziz <lazizjonweb@gmail.com>
2024-02-01 23:59:23 +0500

    delete post is added

--- src/components/Common/Comments/Comment.jsx
+++ src/components/Common/Comments/Comment.jsx
@@ -28,7 +28,7 @@ const Comment = ({ item: comment, postId }) => {
     } catch (error) {
       toast.error(error.message);
     }
-  };
+  };  
 
   const editCommentText = () => {
     setIsEdit(true);
--- src/components/Common/Post/Actions/Actions.jsx
+++ src/components/Common/Post/Actions/Actions.jsx
@@ -3,6 +3,9 @@ import { BsThreeDots } from "react-icons/bs";
 import DropDown from "../../../../utils/DropDown";
 import { useNavigate } from "react-router-dom";
 import { Blog } from "../../../../Context/Context";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../../../firebase/firebase";
+import { toast } from "react-toastify";
 
 const Actions = ({postId, title, desc}) => {
   const { setUpdateData, currentUser } = Blog();
@@ -19,6 +22,33 @@ const Actions = ({postId, title, desc}) => {
     setUpdateData({ title, description: desc });
   };
 
+  const handleRemove = async () => {
+    try {
+      const ref = doc(db, "posts", postId); 
+      const likeRef = doc(db, "posts", postId, "likes", currentUser?.uid);
+      const commentRef = doc(db, "posts", postId, "comments", currentUser?.uid); 
+      const savedPostRef = doc(
+        db,
+        "users",
+        currentUser?.uid,
+        "savePost",
+        postId
+      );
+
+      await deleteDoc(ref);
+      await deleteDoc(likeRef);
+      await deleteDoc(commentRef);
+      await deleteDoc(savedPostRef);
+
+      toast.success("post has been removed");
+      setShowDrop(false);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.success(error.message);
+    }
+  };
+
   return (
     <div className="relative">
       <button onClick={handleClick}>
@@ -26,8 +56,10 @@ const Actions = ({postId, title, desc}) => {
       </button>
       <DropDown showDrop={showDrop} setShowDrop={setShowDrop} size="w-[7rem]">
         <Button click={handleEdit} title="Edit Story" />
-        <Button click="" title="Delete Story" />
+        <Button click={handleRemove} title="Delete Story" />
       </DropDown>
+
+      
     </div>
   );
 };
@@ -35,6 +67,7 @@ const Actions = ({postId, title, desc}) => {
 export default Actions;
 
 const Button = ({ click, title }) => {
+
   return (
     <button
       onClick={click}
